919ea7c7082e1e1a645118a1502060fb5deea41d
Kumar C G <nikhil.kumar.c.g@accenture.com>
2025-04-04 18:26:31 +0530

    cleanup

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -13,10 +13,9 @@ import { CloudTrailStack1 } from '../lib/cloudtrialupdated';
 import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
+
 new PipelineStack(app, 'MultiAccountPipeline', {
   env: { account: '156041406847', region: 'us-east-1' }
 });
-//new EventBusStack1(app, 'EventBusStack1', {});
-//new EventBusDestinationStack(app, 'EventBusDestinationStack', {});
-//new GrafanaCdkStack(app, 'GrafanaCdkStack', {});
-//new CloudTrailStack1(app, 'CloudTrailStack1', {});
+
+
