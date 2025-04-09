0eaebf31b2c7f37bfc2284795cfa5181c377e0df
Kumar C G <nikhil.kumar.c.g@accenture.com>
2025-04-09 12:30:44 +0530

    use github actions

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -1,9 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { EksYamlStack } from '../lib/Manifest';
+import { DeploymentStage } from '../lib/deployment-stage';
 const app = new cdk.App();
  
-new PipelineStack(app, 'MultiAccountPipeline', {
-  env: { account: '156041406847', region: 'us-east-1' }
+// new PipelineStack(app, 'MultiAccountPipeline', {
+//   env: { account: '156041406847', region: 'us-east-1' }
+// });
+
+
+// Source Account Deployment
+new DeploymentStage(app, 'SourceDeployment-820186421740', {
+  env: { account: '820186421740', region: 'us-east-1' }
 });
 
+// Central Account Deployment
+new DeploymentStage(app, 'CentralDeployment', {
+  env: { account: '156041406847', region: 'us-east-1' }
+});
