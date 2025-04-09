70dbe6c3bb2e776294a89caf4ac7d72080322b9b
Kumar C G <nikhil.kumar.c.g@accenture.com>
2025-04-09 12:44:47 +0530

    use github action

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -4,10 +4,6 @@ import { EksYamlStack } from '../lib/Manifest';
 import { DeploymentStage } from '../lib/deployment-stage';
 const app = new cdk.App();
  
-// new PipelineStack(app, 'MultiAccountPipeline', {
-//   env: { account: '156041406847', region: 'us-east-1' }
-// });
-
 
 // Source Account Deployment
 new DeploymentStage(app, 'SourceDeployment-820186421740', {
@@ -18,3 +14,10 @@ new DeploymentStage(app, 'SourceDeployment-820186421740', {
 new DeploymentStage(app, 'CentralDeployment', {
   env: { account: '156041406847', region: 'us-east-1' }
 });
+
+// new PipelineStack(app, 'MultiAccountPipeline', {
+//   env: { account: '156041406847', region: 'us-east-1' }
+// });
+
+
+
--- lib/deployment-stage.ts
+++ lib/deployment-stage.ts
@@ -4,18 +4,23 @@ import { Stage } from 'aws-cdk-lib';
 import { CloudTrailStack } from './cloudtrialupdated';
 import { EventBusDestinationStack } from './eventbusdestination';
 import { EventBusStack } from './eventbussrcupdated';
+import { GrafanaCdkStack } from './grafana'; // Assuming this exists
+
+const CENTRAL_ACCOUNT_ID = '156041406847';
 
 export class DeploymentStage extends Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    if (props?.env?.account === '156041406847') {
-      new EventBusDestinationStack(this, 'EventBusDestinationStack', { env: props.env });
-      //new GrafanaCdkStack(this, 'GrafanaCdkStack', { env: props.env });
+    const account = props?.env?.account;
+    console.log(`Deploying to account: ${account}`);
+
+    if (account === CENTRAL_ACCOUNT_ID) {
+      new EventBusDestinationStack(this, 'EventBusDestinationStack', { env: props?.env ?? {} });
+      //new GrafanaCdkStack(this, 'GrafanaCdkStack', { env: props?.env ?? {} });
     } else {
       new CloudTrailStack(this, 'CloudTrailStack', { env: props?.env ?? {} });  
       new EventBusStack(this, 'EventBusStack', { env: props?.env ?? {} });
-      
     }
   }
 }
