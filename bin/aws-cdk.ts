4bed68f23834a0bedca19362d137a1348796c660
Kumar C G <nikhil.kumar.c.g@accenture.com>
2025-04-11 14:49:18 +0530

    added yaml files

--- bin/aws-cdk.ts
+++ bin/aws-cdk.ts
@@ -2,18 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { EksYamlStack } from '../lib/Manifest';
 import { DeploymentStage } from '../lib/deployment-stage';
+import { CloudTrailStack } from '../lib/cloudtrialupdated';
+import { EventBusStack } from '../lib/eventbussrcupdated';
+import { EventBusDestinationStack } from '../lib/eventbusdestination';
+import { GrafanaCdkStack } from '../lib/grafana'; // Assuming this exists
 const app = new cdk.App();
  
-
+new CloudTrailStack(app, 'CloudTrailStack', {});
+new EventBusStack(app, 'EventBusStack', {});
+new EventBusDestinationStack(app, 'EventBusDestinationStack', {});
+new GrafanaCdkStack(app, 'GrafanaCdkStack', {});
 // Source Account Deployment
-new DeploymentStage(app, 'SourceDeployment-820186421740', {
-  env: { account: '820186421740', region: 'us-east-1' }
-});
+// new DeploymentStage(app, 'SourceDeployment-820186421740', {
+//   env: { account: '820186421740', region: 'us-east-1' }
+// });
 
 // Central Account Deployment
-new DeploymentStage(app, 'CentralDeployment', {
-  env: { account: '156041406847', region: 'us-east-1' }
-});
+// new DeploymentStage(app, 'CentralDeployment', {
+//   env: { account: '156041406847', region: 'us-east-1' }
+// });
 
 // new PipelineStack(app, 'MultiAccountPipeline', {
 //   env: { account: '156041406847', region: 'us-east-1' }
